eb4357c7638fc302f68e3f11f7c6abed615a2326
Arjuna <arjunaragilputera@gmail.com>
2026-04-15 23:23:50 +0700

    fix: sitemap fix

--- ui/src/app/sitemap.ts
+++ ui/src/app/sitemap.ts
@@ -3,13 +3,13 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://v1sualin.com',
+      url: 'https://v1sualin.my.id',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
     },
     {
-      url: 'https://v1sualin.com/contact',
+      url: 'https://v1sualin.my.id/contact',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
